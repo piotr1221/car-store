cfa6488ba938b951df7201cbe13a50a337e0e5de
Edwin Aguilar <edwin.aguilar@unmsm.edu.pe>
2022-02-04 13:39:13 -0500

    initial commit

---
+++ README.md.txt
@@ -0,0 +1,2 @@
+# car-store
+Implementation of the book Design patterns with Java by Laurent Debrawer as a Spring MVC web app
---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+}
